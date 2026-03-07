dc013d5cdb9050f33234ceca9da7172933fd586d
Moataz Ahmed <aymansoltan241@gmail.com>
2026-03-07 22:26:34 +0200

    edit for proc and trg for table intake

--- Moataz/intake/stpAndTrgfortableIntake.sql
+++ Moataz/intake/stpAndTrgfortableIntake.sql
@@ -1,7 +1,7 @@
 use [ExaminationSystemDB]
 go
 create or alter proc [TrainingMangerStp].stp_AddIntake
-    @IntakeName varchar(40)
+    @IntakeName varchar(10)
 as
 begin
     set nocount on;
@@ -9,13 +9,13 @@ begin
         if len(trim(@IntakeName))<2
             throw 50001, 'Error: Intake name must be at least 2 characters long.', 1;
      
-        if exists (select 1 from [orgnization].[Intake] where [IntakeName] = @IntakeName)
+        if exists (select 1 from [Intake] where [IntakeName] = @IntakeName)
             throw 50002, 'Error: This intake name already exists.', 1;
 
-        insert into [orgnization].[Intake] ([IntakeName])
+        insert into [Intake] ([IntakeName])
         values (trim(@IntakeName));
 
-        select SCOPE_IDENTITY() as NewIntakeId;
+        select SCOPE_IDENTITY() as NewIntakeId , 1 as Success , 'intake added Successfully' as Message ;
     end try
     begin catch
         throw;  
@@ -24,28 +24,26 @@ end
 
 go
 create or alter proc [TrainingMangerStp].stp_UpdateIntake
-    @IntakeId int,
-    @IntakeName varchar(20),
-    @IsActive bit =1
+    @IntakeId tinyint,
+    @IntakeName varchar(10)
 as
 begin
     set nocount on;
     begin try
-        if not exists (select 1 from [orgnization].[Intake] where [IntakeId] = @IntakeId and [isDeleted] = 0)
+        if not exists (select 1 from [Intake] where [IntakeId] = @IntakeId and [isDeleted] = 0 )
             throw 50003, 'Error: Intake not found or it has been deleted.', 1;
 
         if len(trim(@IntakeName)) <3
             throw 50001, 'Error: Intake name must be at least 2 characters long.', 1;
 
-         if exists (select 1 from [orgnization].[Intake] where [IntakeName] = @IntakeName and [IntakeId] <> @IntakeId)
+         if exists (select 1 from [Intake] where [IntakeName] = @IntakeName and [IntakeId] <> @IntakeId)
             throw 50002, 'Error: This intake name already exists.', 1;
 
-        update [orgnization].[Intake]
-        set [IntakeName] = trim(@IntakeName),
-            [isActive] = @IsActive
+        update [Intake]
+        set [IntakeName] = trim(@IntakeName)
         where [IntakeId] = @IntakeId;
 
-        select @IntakeId as UpdatedIntakeId;
+        select @IntakeId as UpdatedIntakeId , 1 as Success , 'intake updated Successfully' as Message ;
     end try
     begin catch
         throw;  
@@ -54,16 +52,16 @@ end
 
 go
 
-create or alter proc [TrainingMangerStp].stp_DeleteIntack @IntakeId int
+create or alter proc [TrainingMangerStp].stp_DeleteIntake @IntakeId tinyint 
 as
 begin
     set nocount on;
     begin try
-        if not exists(select 1 from [orgnization].[Intake] where [IntakeId] = @IntakeId and [isDeleted] = 0 )
+        if not exists(select 1 from [Intake] where [IntakeId] = @IntakeId and [isDeleted] = 0 )
             throw 50003, 'Error: Intake not found or it has been deleted.', 1;
 
-        delete from [orgnization].[Intake] where [IntakeId] = @IntakeId
-
+        delete from [Intake] where [IntakeId] = @IntakeId
+        select 1 as Success , 'intake deleted Successfully' as Message ;
     end try
     begin catch
         throw;  
@@ -72,33 +70,33 @@ end
 go
 
 create or alter trigger [orgnization].trg_SoftDeleteIntake
-on [orgnization].[Intake]
+on [Intake]
 instead of delete
 as
 begin
     set nocount on;
-    declare @Intakeid int;
+    declare @Intakeid tinyint;
     select @Intakeid = [IntakeId] from deleted;
 
-    if exists (select 1 from [userAcc].[Student] where [IntakeId] = @Intakeid)
-        or exists(select 1 from [orgnization].[IntakeTrack] where [IntakeId] = @Intakeid )
-        or exists(select 1 from [Courses].[CourseInstance] where [IntakeId] = @Intakeid )
+    if exists (select 1 from [Students] where [IntakeId] = @Intakeid)
+        or exists(select 1 from [IntakeTrack] where [IntakeId] = @Intakeid )
+        or exists(select 1 from [CourseInstance] where [IntakeId] = @Intakeid )
 
     begin
-        update [orgnization].[Intake]
+        update [Intake]
         set [isActive] = 0 , [isDeleted] = 1
         where [IntakeId] = @Intakeid;
         
     end
     else 
     begin
-        delete from [orgnization].[Intake] where [IntakeId] = @Intakeid;
+        delete from [Intake] where [IntakeId] = @Intakeid;
     end
 end
 go
 
 create or alter trigger [orgnization].trg_intakeTrackinactivateWhenInaactiveIntake
-on [orgnization].[Intake]
+on [Intake]
 after update
 as
 begin
@@ -106,11 +104,11 @@ begin
     if exists (select 1 from inserted i join deleted d on i.IntakeId = d.IntakeId 
                where i.isActive = 0 and d.isActive = 1)
     begin
-        declare @intakeid int;
+        declare @intakeid tinyint;
         select @intakeid = IntakeId from inserted;
 
   
-        update [orgnization].[IntakeTrack]
+        update [IntakeTrack]
         set [isActive] = 0 , [isDeleted] = 1
         where [IntakeId] = @intakeid;
     end
