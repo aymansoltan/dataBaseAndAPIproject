2f4d03829f17279d604b420a10e898592df35228
Moataz Ahmed <aymansoltan241@gmail.com>
2026-03-07 22:35:31 +0200

    edit for proc and trg for table intake track

--- Moataz/IntakeTrack/stpAndTrgforTableIntakeTrack.sql
+++ Moataz/IntakeTrack/stpAndTrgforTableIntakeTrack.sql
@@ -2,28 +2,27 @@ use [ExaminationSystemDB]
 
 go
 create or alter proc [TrainingMangerStp].stp_addIntakeTrack
-    @intakeid int,
-    @trackid int
+    @intakeid tinyint,
+    @trackid smallint
 
 as
 begin
     set nocount on;
     begin try
 
-        if not exists (select 1 from [orgnization].[Intake] where [IntakeId] = @intakeid and [isActive] = 1 and [isDeleted] = 0)
+        if not exists (select 1 from [Intake] where [IntakeId] = @intakeid and [isActive] = 1 and [isDeleted] = 0)
             throw 50006, 'Error: Intake not found, inactive, or it has been deleted.', 1;
 
-        if not exists (select 1 from [orgnization].[Track] where [TrackId] = @trackid and [isActive] = 1 and [isDeleted] = 0)
+        if not exists (select 1 from [Track] where [TrackId] = @trackid and [isActive] = 1 and [isDeleted] = 0)
             throw 50007, 'Error: Track not found, inactive, or it has been deleted.', 1;
 
-        if exists (select 1 from [orgnization].[IntakeTrack] where [IntakeId] = @intakeid and [TrackId] = @trackid)
+        if exists (select 1 from [IntakeTrack] where [IntakeId] = @intakeid and [TrackId] = @trackid)
             throw 50008, 'Error: This intake-track relation already exists.', 1;    
 
-        insert into [orgnization].[IntakeTrack] ([IntakeId], [TrackId])
+        insert into [IntakeTrack] ([IntakeId], [TrackId])
         values (@intakeid, @trackid);
 
-
-
+        select @intakeid as intakeId , @trackid as trackId , 1 as Success ,'intake track Added Successfully ' as Message;
     end try
     begin catch
         throw;  
@@ -33,17 +32,17 @@ end
 go
 
 create or alter proc [TrainingMangerStp].stp_ToggleIntakeTrack
-    @intakeid int,
-    @trackid int,
+    @intakeid tinyint,
+    @trackid smallint,
     @status bit
 
 as
 begin
     set nocount on;
-    if not exists (select 1 from [orgnization].[IntakeTrack] where [IntakeId] = @intakeid and [TrackId] = @trackid and [isDeleted] = 0 and [isActive] = 1)
+    if not exists (select 1 from [IntakeTrack] where [IntakeId] = @intakeid and [TrackId] = @trackid and [isDeleted] = 0 )
         throw 50008, 'Error: This intake-track relation not found, inactive, or it has been deleted.', 1;   
 
-    update [orgnization].[IntakeTrack]
+    update [IntakeTrack]
     set [isActive] = @status , [isDeleted] = case when @status = 0 then 1 else 0 end
     where [IntakeId] = @intakeid and [TrackId] = @trackid;
 
@@ -52,46 +51,45 @@ begin
 end
 go
 create or alter proc [TrainingMangerStp].stp_DeleteIntakeTrack
-    @intakeid int,
-    @trackid int
+    @intakeid tinyint ,
+    @trackid smallint
 
 as
 begin
     set nocount on;
     begin try
-        if not exists (select 1 from [orgnization].[IntakeTrack] where [IntakeId] = @intakeid and [TrackId] = @trackid and [isDeleted] = 0)
+        if not exists (select 1 from [IntakeTrack] where [IntakeId] = @intakeid and [TrackId] = @trackid and [isDeleted] = 0)
              throw 50008, 'Error: This intake-track relation not found or it has been deleted.', 1;
 
-        delete from [orgnization].[IntakeTrack] 
+        delete from [IntakeTrack] 
         where [IntakeId] = @intakeid and [TrackId] = @trackid;
-
+        select 1 as Success , 'intake track deleted Successfully' as Message ;
     end try
     begin catch
-throw;
+        throw;
     end catch
 end
 go
 create or alter trigger [orgnization].trg_SoftDeleteIntakeTrack
-on [orgnization].[IntakeTrack]
+on [IntakeTrack]
 instead of delete
 as
 begin
     set nocount on;
-    declare @intakeid int, @trackid int;
+    declare @intakeid tinyint, @trackid smallint;
     
     select @intakeid = [IntakeId], @trackid = [TrackId] from deleted;
 
-    if exists (select 1 from [userAcc].[Student] where [IntakeId] = @intakeid and [TrackId] = @trackid)
-        or exists (select 1 from [Courses].[CourseInstance] where [IntakeId] = @intakeid and [TrackId] = @trackid)
+    if exists (select 1 from [Students] where [IntakeId] = @intakeid and [TrackId] = @trackid)
+        or exists (select 1 from [CourseInstance] where [IntakeId] = @intakeid and [TrackId] = @trackid)
     begin
-        update [orgnization].[IntakeTrack]
+        update [IntakeTrack]
         set [isActive] = 0 , [isDeleted] =1
         where [IntakeId] = @intakeid and [TrackId] = @trackid;
-        
     end
     else 
     begin
-        delete from [orgnization].[IntakeTrack] 
+        delete from [IntakeTrack] 
         where [IntakeId]= @intakeid and [TrackId] = @trackid;
         
     end
